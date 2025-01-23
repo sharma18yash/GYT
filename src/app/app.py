a5f3c8b074ada7510cb4753eb6100a14611dd851
Ashish Chauhan <Ashish.chauhan@students.iiit.ac.in>
2025-01-24 00:05:39 +0530

    - Flask nahin use krna hai

--- src/app/app.py
+++ src/app/app.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
+from service import zerodha_kite_service
+
+
 app = FastAPI()
 
-app.include_router(zeroda_kite_service.router, prefix="/api", tags=["zeroda_kite_service"])
+app.include_router(zerodha_kite_service.router, tags=["zeroda_kite_service"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", log_level="info", reload=True, workers=1, port=8000, host="0.0.0.0")
