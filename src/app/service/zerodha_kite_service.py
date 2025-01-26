285e3303cb938624f24f30eabef05314c8be4c0e
ysharma <sharma18yash.ys@gmail.com>
2025-01-26 14:33:18 +0530

    added imports

--- src/app/service/zerodha_kite_service.py
+++ src/app/service/zerodha_kite_service.py
@@ -9,9 +9,13 @@ _logger = logging.getLogger(__name__)
 
 @router.get("/callback")
 async def callback():
-    return {"message": "Hello from Callback"}
+    return {"message": "Hello from Callback in zerodha-kite-server"}
 
 
 @router.post("/postback")
 async def postback():
     return {"message": "Hello from Postback"}
+
+@router.get("/health")
+async def health_check():
+    return {"status": "ok"}
