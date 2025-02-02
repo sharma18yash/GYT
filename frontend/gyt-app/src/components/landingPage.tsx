b74fa0df5e9da8a28ca1e7697cbd2d515de4e550
Ashish Chauhan <Ashish.chauhan@students.iiit.ac.in>
2025-02-02 16:57:45 +0530

    - Updated react component

--- frontend/gyt-app/src/components/landingPage.tsx
+++ frontend/gyt-app/src/components/landingPage.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
 
-class LandingPage extends React.Component {
-    handleLogin = () => {
+export default function LandingPage() {
+    const handleLogin = () => {
         window.location.href = 'https://gytpay.in/zerodha-kite/login';
     }
 
-    render() {
-        return (
-            <div style={{ textAlign: 'center', marginTop: '50px' }}>
-                <h1>Welcome to Gyt App</h1>
-                <button onClick={this.handleLogin}>Login</button>
-            </div>
-        );
-    }
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to Gyt App</h1>
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
 }
-
-export default LandingPage;
