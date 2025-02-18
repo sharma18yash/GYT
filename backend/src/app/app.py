25a0c30da60ddd7ccf2347bab3a64859e31f8e1d
ysharma <sharma18yash.ys@gmail.com>
2025-02-18 23:24:15 +0530

    Testing automatic deployment

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -9,3 +9,6 @@ app.include_router(zerodha_kite_service.router, tags=["zeroda_kite_service"])
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", log_level="info", reload=True, workers=1, port=8080, host="0.0.0.0")
+
+
+# This is test comment
