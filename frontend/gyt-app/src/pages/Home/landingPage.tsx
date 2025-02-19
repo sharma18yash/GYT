09855ca340f102af661447c1a986fbd74dd9d4e5
Ashish Chauhan <Ashishchauhan7598@gmail.com>
2025-02-20 01:18:58 +0530

    - Executing trade

--- frontend/gyt-app/src/pages/Home/landingPage.tsx
+++ frontend/gyt-app/src/pages/Home/landingPage.tsx
@@ -1,5 +1,7 @@
 
 export default function LandingPage() {
+    console.log('LandingPage');
+
     const handleLogin = () => {
         window.location.href = 'https://gyt.fly.dev/zerodha-kite/login';
     }
